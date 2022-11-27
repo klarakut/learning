041d0a2be82138eec2e17f95e8f6d0b0a3743653
KlaraKutlakova <klarakutlakova@gmail.com>
2022-11-27 13:19:35 +0100

    update SecurityDemoApplication file

--- spring-security/security-demo/src/main/java/learning/securitydemo/SecurityDemoApplication.java
+++ spring-security/security-demo/src/main/java/learning/securitydemo/SecurityDemoApplication.java
@@ -1,7 +1,17 @@
 package learning.securitydemo;
 
+import learning.securitydemo.models.Role;
+import learning.securitydemo.models.User;
+import learning.securitydemo.service.RoleService;
+import learning.securitydemo.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecurityDemoApplication {
@@ -10,4 +20,28 @@ public class SecurityDemoApplication {
 		SpringApplication.run(SecurityDemoApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run (UserService userService, RoleService roleService){
+		return args -> {
+			roleService.saveRole(new Role("ROLE_USER"));
+			roleService.saveRole(new Role("ROLE_MANAGER"));
+			roleService.saveRole(new Role("ROLE_ADMIN"));
+			roleService.saveRole(new Role("ROLE_SUPER_ADMIN"));
+
+			//userService.saveUser(new User("John Travolta","john","john@mail.com","1234", new ArrayList<>()));
+			//userService.saveUser(new User("Will S","will","will@mail.com","1234", new ArrayList<>()));
+			// userService.saveUser(new User("John Travolta","john","1234", new ArrayList<>()));
+			// userService.saveUser(new User("Will S","will","1234", new ArrayList<>()));
+			userService.saveUser(new User("Samantha","sam","1234", new ArrayList<>()));
+
+			// userService.addRoleToUser("john","ROLE_USER");
+			// userService.addRoleToUser("will","ROLE_ADMIN");
+			userService.addRoleToUser("sam","ROLE_USER");
+		};
+	}
 }
