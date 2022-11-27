325e17540edaa0dd84b60f152b300f2da44f6c8e
KlaraKutlakova <klarakutlakova@gmail.com>
2022-11-27 13:18:41 +0100

    create package 'api'

---
+++ spring-security/security-demo/src/main/java/learning/securitydemo/api/RoleToUserForm.java
@@ -0,0 +1,20 @@
+package learning.securitydemo.api;
+
+class RoleToUserForm {
+    private String username;
+    private String roleName;
+
+    public RoleToUserForm(String username, String roleName) {
+        this.username = username;
+        this.roleName = roleName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+}
