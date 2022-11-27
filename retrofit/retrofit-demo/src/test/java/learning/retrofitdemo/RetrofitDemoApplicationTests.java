85bac2cd84c8a14962dfce37ca7ff145c6a5015e
KlaraKutlakova <klarakutlakova@gmail.com>
2022-11-27 15:05:41 +0100

    create project

---
+++ retrofit/retrofit-demo/src/test/java/learning/retrofitdemo/RetrofitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.retrofitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
